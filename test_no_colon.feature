9b91cac7637d5764f5c3976a495b5b4667ac5e14
carlos camara <carlos.camara.galindo@gmail.com>
2026-08-30 16:38:45 +0200

    chore: remove temporary test fixtures

--- test_no_colon.feature
+++
@@ -1,4 +0,0 @@
-Feature Linter testing
-Scenario Missing colon
-Given a step
-And another step
--- test_steps.py
+++
@@ -1,7 +0,0 @@
-from behave import given
-@given('a step')
-def step_impl(context):
-  pass
-@given('another step')
-def step_impl(context):
-  pass
